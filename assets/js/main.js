b3d5b0800cf94f7746ec525606a3e752c08dfa6a
Francesco C <fran461j@outlook.it>
2022-10-24 17:32:11 +0200

    added comment in js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,9 @@ var mailformat = `/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/`;
 let userCredentials = [];
 document.getElementById('signInButton').addEventListener('click', signIn);
 document.getElementById('loginButton').addEventListener('click', login);
+// sign in
 function signIn(){
+    // checking data
     checkBox = document.getElementById("signInCheck").checked;
     checkEmail = document.getElementById("signInEmail").value;
     checkPassword = document.getElementById("signInPassword").value;
@@ -20,15 +22,15 @@ function signIn(){
         document.getElementById('signInAlerts').innerHTML = `<div class="alert alert-danger" role="alert">Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.</div>`
     }
     else{
-        userCredentials = [checkEmail, checkPassword];
-        
+        // succesfull sign in
+        userCredentials = [checkEmail, checkPassword]; 
         console.log(userCredentials);
         document.getElementById('signInEmail').value = "";
         document.getElementById('signInPassword').value = "";
         document.getElementById("signInCheck").checked = false;
     }
 }
-
+// login
 function login(){
     let loginEmailCheck = document.getElementById('loginEmail').value;
     let loginPasswordCheck = document.getElementById('loginPassword').value;
@@ -39,14 +41,12 @@ function login(){
         document.getElementById('loginAlerts').innerHTML = `<div class="alert alert-danger" role="alert">Ops! Something went wrong... Sign In if you're a new in our platform or try to again!</div>`
     }
 }
-
+// game section
 let rolls = [0, 0];
 let scores = [0, 0];
-
+// starting scores
 document.getElementById('userScore').innerHTML = scores[0];
 document.getElementById('aiScore').innerHTML = scores[1];
-
-
 function dices(){
     rolls[0] = Math.floor(Math.random() * 6) + 1;
     rolls[1] = Math.floor(Math.random() * 6) + 1;
@@ -62,17 +62,15 @@ function dices(){
     }
     else if(rolls[0] == rolls[1]){
         document.getElementById('point').innerHTML = 'tie, It could have been worse';
-
     }
     console.log(scores);
     document.getElementById('userScore').innerHTML = scores[0];
     document.getElementById('aiScore').innerHTML = scores[1];
 }
-
+// reset
 function reset(){
     scores[0] = 0;
-    scores[1] = 0;
-    
+    scores[1] = 0;   
     document.getElementById('userScore').innerHTML = scores[0];
     document.getElementById('aiScore').innerHTML = scores[1];
     document.getElementById('userRoll').innerHTML = "";
@@ -80,8 +78,8 @@ function reset(){
     document.getElementById('point').innerHTML = "";
 
 }
+// game events
 document.getElementById('reset').addEventListener('click', reset);
-
 document.getElementById('roll').addEventListener('click', dices);
     
 
